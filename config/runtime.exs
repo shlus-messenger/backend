5e98db5cd43921e6f90e1cfd1793e81836e3855e
Neo <lovchikovmmark@gmail.com>
2026-06-11 18:48:49 +0300

    edit config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,9 +1,12 @@
 import Config
 import Dotenvy
 
-Dotenvy.load!([".env"])
+source!([
+  ".env",
+  System.get_env()
+])
 
-if env("PHX_SERVER") do
+if System.get_env("PHX_SERVER") do
   config :chat, ChatWeb.Endpoint, server: true
 end
 
@@ -11,10 +14,10 @@ config :chat, ChatWeb.Endpoint, http: [ip: {0, 0, 0, 0}, port: String.to_integer
 
 if config_env() == :prod do
   secret_key_base =
-    env("SECRET_KEY_BASE") ||
+    System.get_env("SECRET_KEY_BASE") ||
       raise "environment variable SECRET_KEY_BASE is missing"
 
-  host = env("PHX_HOST") || "localhost"
+  host = System.get_env("PHX_HOST") || "localhost"
 
   config :chat, ChatWeb.Endpoint,
     url: [host: host, port: 4000],
@@ -22,50 +25,50 @@ if config_env() == :prod do
 
 
   config :chat, Chat.Repo,
-    username: env("DB_USER"),
-    password: env("DB_PASSWORD"),
-    database: env("DB_NAME"),
-    hostname: env("DB_HOST"),
-    port: env("DB_PORT"),
+    username: System.get_env("DB_USER"),
+    password: System.get_env("DB_PASSWORD"),
+    database: System.get_env("DB_NAME"),
+    hostname: System.get_env("DB_HOST"),
+    port: System.get_env("DB_PORT"),
     pool_size: 10,
     show_sensitive_data_on_connection_error: true
 
   config :ex_aws,
-  access_key_id: env!("S3_ACCESS_KEY"),
-  secret_access_key: env!("S3_SECRET_KEY"),
+  access_key_id: System.get_env!("S3_ACCESS_KEY"),
+  secret_access_key: System.get_env!("S3_SECRET_KEY"),
   region: "us-east-1"
 
   config :ex_aws, :s3,
-    scheme: env!("S3_SCHEME"),
-    host: env!("S3_HOST"),
-    port: env!("S3_PORT")
+    scheme: System.get_env!("S3_SCHEME"),
+    host: System.get_env!("S3_HOST"),
+    port: System.get_env!("S3_PORT")
 
   config :chat, :redis,
-    url: env!("REDIS_URL")
+    url: System.get_env!("REDIS_URL")
 
 else
 
   config :chat, Chat.Repo,
-    username: env("DEV_DB_USER"),
-    password: env("DEV_DB_PASSWORD"),
-    database: env("DEV_DB_NAME"),
-    hostname: env("DEV_DB_HOST"),
-    port: env("DEV_DB_PORT"),
+    username: System.get_env("DEV_DB_USER"),
+    password: System.get_env("DEV_DB_PASSWORD"),
+    database: System.get_env("DEV_DB_NAME"),
+    hostname: System.get_env("DEV_DB_HOST"),
+    port: System.get_env("DEV_DB_PORT"),
     pool_size: 10,
     show_sensitive_data_on_connection_error: true
 
   config :ex_aws,
-  access_key_id: env!("DEV_S3_ACCESS_KEY"),
-  secret_access_key: env!("DEV_S3_SECRET_KEY"),
+  access_key_id: System.get_env!("DEV_S3_ACCESS_KEY"),
+  secret_access_key: System.get_env!("DEV_S3_SECRET_KEY"),
   region: "us-east-1"
 
   config :ex_aws, :s3,
-    scheme: env!("DEV_S3_SCHEME"),
-    host: env!("DEV_S3_HOST"),
-    port: env!("DEV_S3_PORT")
+    scheme: System.get_env!("DEV_S3_SCHEME"),
+    host: System.get_env!("DEV_S3_HOST"),
+    port: System.get_env!("DEV_S3_PORT")
 
   config :chat, :redis,
-    url: env!("DEV_REDIS_URL")
+    url: System.get_env!("DEV_REDIS_URL")
 
 
 end
